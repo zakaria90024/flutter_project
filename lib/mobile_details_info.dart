c2f65f7b9e49bf382a66b262f11e7523d5c69f2a
Md. Zakaria Ahnaf <zakaria112203@gmail.com>
2022-12-04 15:50:49 +0600

    Normal Search Added On Appbar, search tutorial, total search, Last search, complete tutorial, data and image transferedsdfdsf

--- lib/mobile_details_info.dart
+++ lib/mobile_details_info.dart
@@ -1,21 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'mobile_model.dart';
 
 class MobileInfoScreen extends StatelessWidget {
   var mobileName, mobileUrl, mobileDetails;
 
   MobileInfoScreen(
-      {Key? key, required this.mobileName, required this.mobileUrl, required this.mobileDetails})
+      {Key? key,
+      required this.mobileName,
+      required this.mobileUrl,
+      required this.mobileDetails})
       : super(key: key);
 
   //final String mobileName1 = mobileName;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(mobileName + mobileUrl),
-      ),
-    );
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SafeArea(
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text(mobileName),
+            ),
+            body: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Center(
+                    child: Row(
+                      children: [
+                        Image.network(mobileUrl),
+                      ],
+                    ),
+                  ),
+                  Center(
+                    child: Row(
+                      children: [Html(data: mobileDetails)],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
